8224d3b01509406d3f90d441f9c1d9d6193cf7d6
avi9984 <avinashkumar151199@gmail.com>
2026-06-22 21:31:33 +0530

    css property

--- introduction to js/style.js
+++ introduction to js/style.js
@@ -1,4 +1,18 @@
-console.log("Hello World");
-console.error("Hello Error");
-console.warn("Hello Warn");
-console.info("Hello Info");
+// console.log("Hello World");
+// console.error("Hello Error");
+// console.warn("Hello Warn");
+// console.info("Hello Info");
+
+// ( 10 + 20 + 30 + 40 + 50 ) / 100 = ?
+
+
+var a = 10;
+var b = 20;
+var c = 30;
+var d = 40
+var e = 50;
+var f = 100;
+
+var result = (a + b + c + d + e )/ f
+console.log(result);
+
