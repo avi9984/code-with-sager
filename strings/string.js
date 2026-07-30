d148aa688cfe3594f865bf46dc13ab225b0cba32
avi9984 <avinashkumar151199@gmail.com>
2026-07-30 22:22:28 +0530

    strings/string.js

---
+++ strings/string.js
@@ -0,0 +1,23 @@
+// let num = 5;
+
+// for (let i = 1; i <= 10; i++) {
+
+//     console.log(`${num} x ${i} = ${num * i}`);
+
+// }
+
+let x = "Avinash is a good boy Avinash";
+
+console.log(x.toLowerCase());
+console.log(x.toUpperCase());
+console.log(x.startsWith("Avi"));
+console.log(x.endsWith("good"));
+console.log(x.replace("Avinash", "simple"));
+console.log(x.replaceAll("Avinash", "simple"));
+console.log(x.charAt(10));
+console.log(x.split(" "));
+
+
+
+
+
