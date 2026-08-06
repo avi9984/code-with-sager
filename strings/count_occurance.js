5319feda8d87873359db0cfddde8d333569372a3
avi9984 <avinashkumar151199@gmail.com>
2026-08-06 21:34:47 +0530

    strings/count_occurance.js

---
+++ strings/count_occurance.js
@@ -0,0 +1,12 @@
+
+
+function countOccurance(str){
+    let freq={};
+
+    for(let char of str){
+        freq[char]=(freq[char]||0)+1
+    }
+    return freq
+}
+
+console.log(countOccurance("coding wise"));
