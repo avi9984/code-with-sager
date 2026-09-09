3da9396ae92d7265bc59ed84bbc680cd6f831957
avi9984 <avinashkumar151199@gmail.com>
2026-09-09 20:13:15 +0530

    localstorage method in

--- BOM/script.js
+++ BOM/script.js
@@ -1,8 +1,20 @@
 
 
-localStorage.setItem("name", "Avinash")
-localStorage.setItem("email", "avi@gmail.com")
-localStorage.setItem("rollno", 100)
+// localStorage.setItem("name", "Avinash")
+// localStorage.setItem("email", "avi@gmail.com")
+// localStorage.setItem("rollno", 100)
+
+
+// const dialog=document.getElementById('my-model');
+// const 
+
+const newApi = async () => {
+    const result = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=e981b152dfc44819b691e8f45defea88')
+    const json = await result.json()
+    console.log(json)
+}
+
+newApi()
 
 
 
